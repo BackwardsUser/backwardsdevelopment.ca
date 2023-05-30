80f0b6dbe38f7d6f124338c5a3e79f359ccef128
Backwards <AlexHardingYT@gmail.com>
2023-05-30 14:42:00 -0400

    Caching sucks.

--- src/public/scripts/discordLogin.js
+++ src/public/scripts/discordLogin.js
@@ -1,3 +1,3 @@
 function loginViaDiscord() {
-    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=723670738057560085&redirect_uri=https%3A%2F%2Fbackwardsdevelopment.ca%2Fauth%2Fdiscord&response_type=code&scope=identify"
+    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=723670738057560085&redirect_uri=https%3A%2F%2Fbackwardsdevelopment.ca%2Fauth%2Fdiscord&response_type=token&scope=identify"
 }
