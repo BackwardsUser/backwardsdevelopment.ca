27adab391ecee4285fe58c42d86da2d602d0ae59
Backwards <49242785+BackwardsUser@users.noreply.github.com>
2023-05-31 00:30:35 -0400

    Added Options

--- src/public/scripts/discordLogin.js
+++ src/public/scripts/discordLogin.js
@@ -1,5 +1,3 @@
-var production = true;
-
 function loginViaDiscord() {
     if (production == false) {
         window.location.href = "https://discord.com/api/oauth2/authorize?client_id=723670738057560085&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord&response_type=token&scope=identify";
